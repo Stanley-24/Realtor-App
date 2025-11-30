51623c7b1592dd95017053f0be746fb38ae0145d
Stanley <owarieta24@outlook.com>
2025-11-30 03:36:25 +0100

    feat: added google client id to env and import it to our client config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,10 @@
 interface ApiConfig {
   API_URL: string;
+  VITE_GOOGLE_CLIENT_ID: string;
 }
 
 
 export const apiConfig: ApiConfig = {
-   API_URL :  import.meta.env.VITE_API_URL || "http://localhost:5000"
+   API_URL :  import.meta.env.VITE_API_URL || "http://localhost:5500",
+   VITE_GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID || "",
 }
