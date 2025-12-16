2dfd9a827b31e7cffe020941458a2b3dc797332b
Stanley <owarieta24@outlook.com>
2025-12-16 01:34:35 +0100

    seprate code so startup has it's seprate files and also import app for unit testing

---
+++ server/src/app.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import config from "./config/config";
+import authRoutes from "./routes/auth.routes";
+import propertyRoutes from "./routes/property.routes";
+import healthRoutes from "./routes/health.checker"
+import googleAuthRoutes from "./routes/googleAuth.routes";
+import path from "path";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow cookies to be sent/received
+  })
+);
+
+// Set Cross-Origin-Opener-Policy header to allow Google OAuth postMessage
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
+  next();
+});
+
+// 🔧 Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// health checker
+app.use("/api/v1", healthRoutes);
+
+app.use("/api/v1/auth", googleAuthRoutes);
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/properties", propertyRoutes);
+
+
+
+if (config.nodeEnv === "production") {
+  const clientPath = path.join(__dirname, "../../client/dist");
+
+  app.use(express.static(clientPath));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
+
+export default app;
