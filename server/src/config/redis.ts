68b946ef07ff6905b6baa7b9633afd164aa6b4df
Stanley <owarieta24@outlook.com>
2025-12-17 13:36:01 +0100

    set enableofflineque to true

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -2,7 +2,7 @@
 import Redis from 'ioredis';
 
 const redisClient = new Redis(process.env.REDIS_URL || 'redis://localhost:6379', {
-  enableOfflineQueue: false,
+  enableOfflineQueue: true,
 });
 
 redisClient.on('error', (err) => {
