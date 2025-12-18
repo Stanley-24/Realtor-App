d705063dbbdc276cb4e052cee4542e2115a0be20
Stanley <owarieta24@outlook.com>
2025-12-18 22:20:56 +0100

    remove trimming from model and trim manually in controller

--- server/src/models/contactMessage.model.ts
+++ server/src/models/contactMessage.model.ts
@@ -12,12 +12,10 @@ const ContactMessageSchema = new Schema<IContactMessage>(
     fullName: {
       type: String,
       required: [true, 'Full name is required'],
-      trim: true,
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
-      lowercase: true,
       match: [
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         'Please use a valid email address',
@@ -26,7 +24,6 @@ const ContactMessageSchema = new Schema<IContactMessage>(
     message: {
       type: String,
       required: [true, 'Message is required'],
-      minlength: [10, 'Message must be at least 10 characters'],
     },
   },
   { timestamps: true }
