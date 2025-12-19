7547ca143a9d23cd9bec4a4622ccb74e02dac4c7
Stanley <owarieta24@outlook.com>
2025-12-19 18:33:08 +0100

    add rate limiter to contact form route

--- server/src/routes/contactForm.routes.ts
+++ server/src/routes/contactForm.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { submitContactForm } from "../controllers/user/contactForm.controller";
+import { contactFormLimiter } from "../middlewares/rateLimiterContact";
 
 const router = Router();
 
-router.post("/contact", submitContactForm);
+router.post("/contact", contactFormLimiter, submitContactForm);
 
 export default router;
