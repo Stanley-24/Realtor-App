c43ffad0a13c46cd872f8f441c4df94b49af7e0b
Stanley <owarieta24@outlook.com>
2025-12-28 20:22:04 +0100

    feat: route register route for the create property page and mock the rest path for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,9 @@ import { useAuthStore } from '@/store/authStore'
 import SelectRolePage from '@/pages/Auth/SelectRolePage'
 import ResetPasswordPage from '@/pages/Auth/resetPass'
 import ForgotPasswordPage from '@/pages/Auth/forgetPass'
+import CreatePropertyPage from './pages/Dashboard/agent/components/createProperty'
+import { Toaster } from 'sonner';
+
 
 function App() {
   const { checkAuth } = useAuthStore();
@@ -21,27 +24,78 @@ function App() {
   }, [checkAuth]);
 
   return (
-    <Routes>
-      <Route path='/' element={< HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignupPage />} />
-      <Route path="/select-role" element={<SelectRolePage />} />
-      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-      <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+    <>
+      <Routes>
+        <Route path='/' element={< HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path="/select-role" element={<SelectRolePage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
 
 
-      <Route element={<ProtectedRoute allowedRoles={["admin"]}/> }>
-        <Route path='/dashboard/admin' element={<AdminDashboard />} />
-      </Route>
+        <Route element={<ProtectedRoute allowedRoles={["admin"]}/> }>
+          <Route path='/dashboard/admin' element={<AdminDashboard />} />
+        </Route>
 
-      <Route element={<ProtectedRoute allowedRoles={["agent"]}/> }>
-        <Route path='/dashboard/agent' element={<AgentDashboard />} />
-      </Route>
+        <Route element={<ProtectedRoute allowedRoles={["agent"]} />}>
+          <Route path="/dashboard/agent" element={<AgentDashboard />}>
+            <Route index element={<CreatePropertyPage />} /> 
+            {/* or AgentHome later */}
 
-      <Route element={<ProtectedRoute allowedRoles={["buyer"]}/> }>
-        <Route path='/dashboard/buyer' element={<BuyerDashboard />} />
-      </Route>
-    </Routes>
+            <Route
+              path="create-property"
+              element={<CreatePropertyPage />}
+            />
+
+            <Route
+              path="my-listings"
+              element={<div>My Listings</div>}
+            />
+
+            <Route
+              path="analytics"
+              element={<div>Analytics</div>}
+            />
+
+            <Route
+              path="inbox"
+              element={<div>Inbox</div>}
+            />
+
+            <Route
+              path="update-listing"
+              element={<div>Update Listing</div>}
+            />
+
+            <Route
+              path="recently-deleted"
+              element={<div>Recently Deleted</div>}
+            />
+          </Route>
+        </Route>
+
+
+        <Route element={<ProtectedRoute allowedRoles={["buyer"]}/> }>
+          <Route path='/dashboard/buyer' element={<BuyerDashboard />} />
+        </Route>
+
+      </Routes>
+
+      <Toaster 
+        position="top-center"   // or top-right, bottom-center, etc.
+        richColors              // nice colored toasts (success green, error red)         
+        duration={3000}  // auto-dismiss after 3
+        toastOptions={{
+          style: {
+            background: "#028100",
+            color: 'white',
+            fontSize: '16px',
+            fontFamily: "Arial, sans-serif",
+          },
+        }}
+      />
+    </>
   )
 }
 
