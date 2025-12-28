7c9d754ae3c105a9949b4bbf9b0661e3747cfde5
Stanley <owarieta24@outlook.com>
2025-12-28 20:26:26 +0100

    export cn function for tailwind merge

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,3 +1,12 @@
+
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
 /**
  * Get dashboard URL based on user role
  */
